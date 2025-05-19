17bf4dc862672756b7e1ae4b1abbd332b45b0cdd
Frederico Simas Duarte <fredmdfk@gmail.com>
2025-05-19 23:31:18 +0200

    Draws rectangles on a canvas
    Initializes the Canvas screen and view model.
    
    Sets up a basic canvas to display rectangles with a configurable
    height and stroke color.
    
    Introduces a view state to manage the canvas properties and rectangle
    data.

--- app/src/main/java/com/simplified/canvas/ui/screen/CanvasScreen.kt
+++ app/src/main/java/com/simplified/canvas/ui/screen/CanvasScreen.kt
@@ -1,17 +1,65 @@
 package com.simplified.canvas.ui.screen
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.simplified.canvas.navigationgraph.MainGraph
 import com.simplified.canvas.ui.viewmodel.CanvasViewModel
+import com.simplified.canvas.ui.viewmodel.ViewState
 
 @Composable
 @Destination<MainGraph>(start = true)
 fun CanvasScreen(
     viewModel: CanvasViewModel = hiltViewModel(),
 ) {
+    val viewState by viewModel.viewState.collectAsStateWithLifecycle()
+
+    CanvasScreenContent(
+        viewState = viewState,
+    )
+}
+
+@Composable
+fun CanvasScreenContent(
+    modifier: Modifier = Modifier,
+    viewState: ViewState,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center,
+    ) {
+        Canvas(
+            modifier = Modifier
+                .clipToBounds()
+                .fillMaxWidth()
+                .requiredHeight(viewState.minCanvasHeight.dp)
+                .background(Color.White),
+        ) {
+            for (rectangle in viewState.rectangles) {
+                drawRect(
+                    color = viewState.strokeColor,
+                    topLeft = rectangle.rect.topLeft,
+                    size = rectangle.rect.size,
+                )
+            }
+        }
+    }
 }
 
 @Preview
--- app/src/main/java/com/simplified/canvas/ui/viewmodel/CanvasViewModel.kt
+++ app/src/main/java/com/simplified/canvas/ui/viewmodel/CanvasViewModel.kt
@@ -1,10 +1,94 @@
 package com.simplified.canvas.ui.viewmodel
 
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @HiltViewModel
 class CanvasViewModel @Inject constructor(
 ): ViewModel() {
+
+    private val _viewState = MutableStateFlow(
+        ViewState(
+            strokeColor = Color.Black,
+            minCanvasHeight = 0,
+            currentTopZIndex = 0,
+            rectangles = emptyList(),
+        )
+    )
+    val viewState: StateFlow<ViewState> = _viewState.asStateFlow()
+
+    init {
+        _viewState.value = ViewState(
+            strokeColor = Color.Black,
+            minCanvasHeight = 400,
+            currentTopZIndex = 2,
+            rectangles = createRectangles(5, 200f, 200f),
+        )
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    private fun createNewRectangle(
+        topLeftX: Float,
+        topLeftY: Float,
+        width: Float,
+        height: Float,
+    ): Rectangle {
+        return Rectangle(
+            id = Uuid.random().toString(),
+            rect = Rect(
+                topLeftX,
+                topLeftY,
+                topLeftX + width,
+                topLeftY + height,
+            ),
+            zIndex = _viewState.value.currentTopZIndex + 1,
+        )
+    }
+
+    private fun createRectangles(
+        n: Int = 3,
+        rectangleWidth: Float = 100f,
+        rectangleHeight: Float = 100f,
+    ): List<Rectangle> {
+        val rectangles = mutableListOf<Rectangle>()
+        var currentX = 0f
+        var currentY = 0f
+
+        for (i in 1..n) {
+            rectangles.add(
+                createNewRectangle(
+                    topLeftX = currentX,
+                    topLeftY = currentY,
+                    width = rectangleWidth,
+                    height = rectangleHeight,
+                )
+            )
+
+            currentX += rectangleWidth
+            currentY += rectangleHeight
+        }
+
+        return rectangles
+    }
 }
+
+data class ViewState(
+    val strokeColor: Color,
+    val minCanvasHeight: Int,
+    val currentTopZIndex: Int, // Remove this if not needed
+    val rectangles: List<Rectangle>,
+)
+
+data class Rectangle(
+    val id: String,
+    val rect: Rect,
+    val zIndex: Int,
+)
